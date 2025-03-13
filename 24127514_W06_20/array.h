39aabec61aa271c842181a40991351b11a4f58c7
phn0801 <ngophong0801@gmail.com>
2025-03-13 21:42:22 +0700

    Update array.h

--- 24127514_W06_20/array.h
+++ 24127514_W06_20/array.h
@@ -1,2 +1,14 @@
 #pragma once
- abf
+#include<iostream>
+
+using namespace std;
+ void P01(int a[], int n){
+    int so_chan_max = -100;
+    int so_le_max = -101;
+    for(int i = 0; i < n; i++){
+        if(a[i] % 2 == 0 ){
+            if(a[i] > so_chan_max) so_chan_max = a[i];
+            
+        }
+    }
+}
