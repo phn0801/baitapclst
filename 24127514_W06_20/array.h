d787ec2ccd5033aeadd7526eb536775a361d0d27
phn0801 <ngophong0801@gmail.com>
2025-03-13 21:54:49 +0700

    Update array.h

--- 24127514_W06_20/array.h
+++ 24127514_W06_20/array.h
@@ -2,13 +2,13 @@
 #include<iostream>
 
 using namespace std;
- void P01(int a[], int n){
-    int so_chan_max = -100;
-    int so_le_max = -101;
-    for(int i = 0; i < n; i++){
-        if(a[i] % 2 == 0 ){
-            if(a[i] > so_chan_max) so_chan_max = a[i];
-            
-        }
-    }
-}
+void P01(int a[], int n);
+void P04(int a[], int n);
+void P06(int a[], int n);
+int P12(int a[], int n);
+void P25(int a[], int b[], int na, int nb);
+double P320(int a[], int n);
+int P347(int a[], int n);
+void P402(int a[], int n);
+int P438(int a[], int n);
+void P458(int a[], int n);
